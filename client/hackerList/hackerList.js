7beeef9de95c4f23bbb97f96e29533f83d4cea8e
JonDuarteBr <jonrodriguesbr@gmail.com>
2016-04-11 19:58:57 -0400

    add subscribe and publish

--- client/hackerList/hackerList.js
+++ client/hackerList/hackerList.js
@@ -1,6 +1,10 @@
 
 Template.hackerList.helpers({
     'Hackers':function (){
-      return Hackers.find ();
-    }
-});
+      return Hackers.find }();
+
+}); 
+
+Template.hackerList.OnCreated(function(){
+	Meteor.subsribe('hackers')
+	});
